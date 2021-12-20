b6e1a69378377b0629dbcc87c63b147cd6693162
Murali <91236773+muralitheprogrammer@users.noreply.github.com>
2021-12-20 11:01:59 +0530

    Update connection.py
    no need to check

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,3 +1,4 @@
+#MW Bros
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
